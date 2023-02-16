940654aedc976cb1d5c5c6d781e66cb864f5c096
Alekei Sobolevskii <aleksei_sobolevskii@intuit.com>
2023-02-16 15:48:36 -0500

    Initial Commit

---
+++ DesignCodeiOS16/DesignCodeiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS16App.swift
+//  DesignCodeiOS16
+//
+//  Created by Aleksei Sobolevskii on 2023-02-16.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
